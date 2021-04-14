0fd7b274f28d39be87ac36331095331b31d246e4
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-04-14 17:32:18 -0400

    Add files via upload

---
+++ Ejemplo/Cargador.java
@@ -0,0 +1,14 @@
+
+public class Cargador
+{
+    private int voltios;
+    private String marca;
+    
+    public Cargador(int voltios, String marca){
+        this.voltios = voltios;
+        this.marca = marca;
+    }
+    
+    //sus metodos
+    //sus get y set
+}
